53423a632278fb2c55d8646d4f0fb2175d848c36
Toan Nguyen <n.minhtoan@gmail.com>
2019-05-24 20:03:08 -0700

    update api host

--- web/api/index.js
+++ web/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = (token = '') => axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: `${process.env.API_HOST}/api`,
   headers: {
     'x-access-token': token
   }
